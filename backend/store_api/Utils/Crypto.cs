7400fd38de0a98d570bd9e6c209984d7aac66f42
Filipe Junqueiro <filipe.junqueiro.2005@gmail.com>
2025-12-14 23:34:13 +0000

    fix: Minor Fixes on Frontend & Backend

---
+++ backend/store_api/Utils/Crypto.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace mongo_api.Utils;
+
+public class Crypto
+{
+    public static string ToHexString(string input)
+    {
+        var inBytes = Encoding.UTF8.GetBytes(input);
+        var inHash = SHA3_256.Create().ComputeHash(inBytes);
+        return Convert.ToHexString(inHash);
+    }
+
+    public static bool IsHexStringValid(string input, string hex)
+    {
+        var inHex = ToHexString(input);
+        return hex.Equals(inHex);
+    }
+}
